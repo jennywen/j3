b63e8383cd66ff6d9ada9ec796a5f29dd8d25341
Juliet Liu <julietliu94@gmail.com>
2014-09-20 22:33:43 -0400

    stuff

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,24 +4,6 @@
 
 var dirtyChoreControllers = angular.module('dirtyChoreControllers', []);
 
-dirtyChoreControllers.controller('HomeCtrl', ['$scope', 'User',
-  function($scope, User) {
-    $scope.users = User.query();
-    $scope.orderProp = 'name';
-  }]);
-
-dirtyChoreControllers.controller('ProfileCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
-
-dirtyChoreControllers.controller('LboardCtrl', ['$scope', 
-  function($scope) {
-    //populate list of objects { person, points}
+dirtyChoreControllers.controller('HomeCtrl', ['$scope', function($scope) {
+    $scope.users = [{name:"Bob"}];
   }]);
