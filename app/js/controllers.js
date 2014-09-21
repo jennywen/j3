47456efb3a70ac3aec04adb2b8638f0e0c462d41
Juliet Liu <julietliu94@gmail.com>
2014-09-21 03:04:57 -0400

    Fixed controllers  Please enter the commit message for your changes. Lines starting

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,91 @@
 
 var dirtyChoreControllers = angular.module('dirtyChoreControllers', []);
 
-dirtyChoreControllers.controller('HomeCtrl', ['$scope', function($scope) {
-    $scope.users = [{name:"Bob"}, {name:"Joe"}];
-  }]);
+/* Home Page */
+dirtyChoreControllers.controller('HomeCtrl', ['$rootScope', 'UserService', 
+  function($rootScope, UserService) {
+    $rootScope.showLeaderboardView = false;
+    $rootScope.showProfileView = false;
+    $rootScope.users = [{
+      id: 0,
+      name: "Jenny",
+      score: 0,
+      currentChore: [0, -1]
+    },
+    {
+      id: 1,
+      name: "Juliet",
+      score: 0,
+      currentChore: [1, -1]
+    },
+    {
+      id: 2,
+      name: "Jad",
+      score: 0,
+      currentChore: [2, -1]
+    },
+    {
+      id: 3,
+      name: "James",
+      score: 0,
+      currentChore: [3, -1]
+    },
+    {
+      id: 4,
+      name: "Jarrett",
+      score: 0,
+      currentChore: [4, -1]
+    }];
+
+    $rootScope.chores = [
+    {
+      name: "Garbage",
+      description: "Take out trash and recycling when necessary."
+    },
+    {
+      name: "Bathroom",
+      description: "Clean toilet bowl and surfaces."
+    },
+    {
+      name: "Floors",
+      description: "Sweep and vacuum."
+    },
+    {
+      name: "Countertops",
+      description: "Wipe down with clean towel."
+    },
+    {
+      name: "Dishes",
+      description: "Wash thoroughly."
+    }];
+
+    // var promise = new Promise(function(resolve) {
+    //   UserService.success(function(data) {
+    //     console.log(data);
+    //     resolve(data);
+    //   });
+    // });
+
+    // promise.then(function(result){
+    //   $rootScope.users = result;
+    // });
+
+    $rootScope.changeProfileView = function(userId){
+      $rootScope.currentUserId = userId; // available to other controllers
+      $rootScope.showProfileView = true;
+    };
+}]);
+
+/* Profile Page */
+dirtyChoreControllers.controller('ProfileCtrl', ['$rootScope', function($rootScope) {
+
+  $rootScope.changeLeaderBdView = function(user){
+     $rootScope.showLeaderboardView = true;
+     $rootScope.showProfileView = false;
+  };
+}]);
+
+/* Leaderboard Page */
+dirtyChoreControllers.controller('LeaderboardCtrl', ['$rootScope', function($rootScope) {
+  $rootScope.varb = '';
+}]);
